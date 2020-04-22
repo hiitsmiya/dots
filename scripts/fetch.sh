3d0e670c024aa56d2343c9987c310830193bae04
deven <56056447+notdeven@users.noreply.github.com>
2020-04-22 00:22:07 -0700

    imagine using neofetch L O L :wheelchair:

--- scripts/fetch.sh
+++ scripts/fetch.sh
@@ -3,11 +3,11 @@ cat <<a
 
 
 
-         os > $(grep PRETTY /etc/os-release | cut -c 13- | tr -d '"' | tr '[A-Z]' '[a-z]')
-         model > $(cat /sys/devices/virtual/dmi/id/product_family | tr '[A-Z]' '[a-z]')
-         pkgs > $(pacman -Q | wc -l)
-         uptime > $(uptime --pretty | tr -d 'upourinutes ' | tr ',' ' ')
-         ram > $(free -h | sed -n '2{p;q}' | awk '{print $3 "/" $2}' | tr -d 'i' | tr '[A-Z]' '[a-z]')
+         os: $(grep PRETTY /etc/os-release | cut -c 13- | tr -d '"' | tr '[A-Z]' '[a-z]')
+         model: $(cat /sys/devices/virtual/dmi/id/product_family | tr '[A-Z]' '[a-z]')
+         pkgs: $(pacman -Q | wc -l)
+         uptime: $(uptime --pretty | tr -d 'upourinutes ' | tr ',' ' ')
+         ram: $(free -h | sed -n '2{p;q}' | awk '{print $3 "/" $2}' | tr -d 'i' | tr '[A-Z]' '[a-z]')
 
 
 a
