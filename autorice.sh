4e3f23a97fd349b4268a05bffc97ec7401479a50
deven <56056447+notdeven@users.noreply.github.com>
2020-04-23 01:20:53 -0700

    1

--- autorice.sh
+++ autorice.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-cp -r $HOME/Documents/dots/.zshrc $HOME/.zshrc
+cp -r $HOME/Documents/dots/.zshrc $HOME/
 cp -r $HOME/Documents/dots/config $HOME/.config/i3/
 cp -r $HOME/Documents/dots/i3blocks.conf /etc/
 
 cp -r $HOME/Documents/dots/Wallpapers ~/Pictures/
-CP -r $HOME/Documents/dots/i3blocks/ ~/.config/ | chmod +x ~/.config/scripts/fetch | chmod +x ~/.config/scripts/panes.sh
+cp -r $HOME/Documents/dots/i3blocks/ ~/.config/ | chmod +x ~/.config/scripts/fetch | chmod +x ~/.config/scripts/panes.sh
 | chmod +x ~/.config/scripts/pipes.sh
 
