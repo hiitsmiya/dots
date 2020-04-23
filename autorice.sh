f5002744f4db3b7fb5526fced4da05dfa35ee9e9
deven <56056447+notdeven@users.noreply.github.com>
2020-04-23 01:28:26 -0700

    getting there

--- autorice.sh
+++ autorice.sh
@@ -5,6 +5,12 @@ cp -r $HOME/Documents/dots/config $HOME/.config/i3/
 cp -r $HOME/Documents/dots/i3blocks.conf /etc/
 
 cp -r $HOME/Documents/dots/Wallpapers ~/Pictures/
-cp -r $HOME/Documents/dots/i3blocks/ ~/.config/ | chmod +x ~/.config/scripts/fetch | chmod +x ~/.config/scripts/panes.sh
-| chmod +x ~/.config/scripts/pipes.sh
+cp -r $HOME/Documents/dots/i3blocks/ ~/.config/
+chmod +x ~/.config/i3blocks/battery2.py
+chmod +x ~/.config/i3blocks/volume
+chmod +x ~/.config/i3blocks/corona
 
+cp $HOME/Documents/dots/scripts/ $HOME/.config/
+chmod +x ~/.config/scripts/fetch
+chmod +x ~/.config/scripts/panes.sh
+chmod +x ~/.config/scripts/pipes.sh
