4786224aaedfc98433f94714d12512edf93452c5
deven <56056447+notdeven@users.noreply.github.com>
2020-04-23 01:30:28 -0700

    getting closer

--- autorice.sh
+++ autorice.sh
@@ -2,7 +2,7 @@
 
 cp -r $HOME/Documents/dots/.zshrc $HOME/
 cp -r $HOME/Documents/dots/config $HOME/.config/i3/
-cp -r $HOME/Documents/dots/i3blocks.conf /etc/
+## doesn't wanna work atm cp -r $HOME/Documents/dots/i3blocks.conf /etc/
 
 cp -r $HOME/Documents/dots/Wallpapers ~/Pictures/
 cp -r $HOME/Documents/dots/i3blocks/ ~/.config/
@@ -10,7 +10,7 @@ chmod +x ~/.config/i3blocks/battery2.py
 chmod +x ~/.config/i3blocks/volume
 chmod +x ~/.config/i3blocks/corona
 
-cp $HOME/Documents/dots/scripts/ $HOME/.config/
+cp -r $HOME/Documents/dots/scripts/ $HOME/.config/
 chmod +x ~/.config/scripts/fetch
 chmod +x ~/.config/scripts/panes.sh
 chmod +x ~/.config/scripts/pipes.sh
