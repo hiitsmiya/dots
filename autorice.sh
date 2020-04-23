5f78f34cb6abe1ad2ba4912a0581c7062bbece56
deven <56056447+notdeven@users.noreply.github.com>
2020-04-23 02:57:54 -0700

    only use if you know what everything does.

--- autorice.sh
+++ autorice.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
 yay -S brave-bin python-pywal python-pip ranger dunst redshift acpi i3blocks i3-gaps kitty scrot udiskie xorg-xbacklight ttf-font-awesome zsh
+pip install pillow
+pip install yaishoku
+yay -Rcns python-pip
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 
 cp -r $HOME/Documents/dots/Wallpapers ~/Pictures/
@@ -24,22 +28,15 @@ cp -r $HOME/Documents/dots/scripts/ $HOME/.config/
 chmod +x $HOME/.config/scripts/fetch
 chmod +x $HOME/.config/scripts/panes.sh
 chmod +x $HOME/.config/scripts/pipes.sh
-cp -r $HOME/Documents/dots/.zshrc $HOME/
 
 # terminal and file stuff
 mkdir -p $HOME/.config/kitty
-cp -r $HOME/Documents/dots/kitty.conf $/HOME/.config/kitty/
+cp -r $HOME/Documents/dots/kitty.conf $HOME/.config/kitty/
 mkdir -p $HOME/.config/ranger
 cp -r $HOME/Documents/dots/rc.conf $HOME/.config/ranger/
 
 cp +r $HOME/Documents/dots/.vimrc $HOME/
 sudo cp +r $HOME/Documents/dots/.xinitrc $HOME/
 
-pip install pillow
-pip install yaishoku
-yay -Rcns python-pip
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-
-echo "done, enjoy your rice. use 'startx' to get hopping!"
+echo "done, enjoy your rice. use 'startx' to get hopping."
