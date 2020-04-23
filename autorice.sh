eb98c930496cfdd8c2c6a8bdb920be7c696b080f
deven <56056447+notdeven@users.noreply.github.com>
2020-04-23 01:52:20 -0700

    ONLY USE IF YOU KNOW WHAT YOU'RE DOING

--- autorice.sh
+++ autorice.sh
@@ -1,19 +1,29 @@
 #!/bin/sh
 
-cp -r $HOME/Documents/dots/.zshrc $HOME/
-cp -r $HOME/Documents/dots/config $HOME/.config/i3/
-## doesn't wanna work atm cp -r $HOME/Documents/dots/i3blocks.conf /etc/
+
 
 cp -r $HOME/Documents/dots/Wallpapers ~/Pictures/
-cp -r $HOME/Documents/dots/i3blocks/ ~/.config/
-chmod +x ~/.config/i3blocks/battery2.py
-chmod +x ~/.config/i3blocks/volume
-chmod +x ~/.config/i3blocks/corona
+cp -r $HOME/Documents/dots/config $HOME/.config/i3/
 
+# dunst stuff
+cp -r $HOME/Documents/dots/dunst/ $HOME/.config/
+chmod +x $HOME/.config/dunst/launch.sh
+sudo cp -r $HOME/Documents/dots/dunstwal /bin/
+sudo chmod +x /bin/dunstwal
+
+# i3blocks stuff
+cp -r $HOME/Documents/dots/i3blocks/ ~/.config/
+chmod +x $HOME/.config/i3blocks/battery2.py
+chmod +x $HOME/.config/i3blocks/volume
+chmod +x $HOME/.config/i3blocks/corona
+sudo cp -r $HOME/Documents/dots/i3blocks.conf /etc/
+
+# terminal stuff
 cp -r $HOME/Documents/dots/scripts/ $HOME/.config/
-chmod +x ~/.config/scripts/fetch
-chmod +x ~/.config/scripts/panes.sh
-chmod +x ~/.config/scripts/pipes.sh
+chmod +x $HOME/.config/scripts/fetch
+chmod +x $HOME/.config/scripts/panes.sh
+chmod +x $HOME/.config/scripts/pipes.sh
+cp -r $HOME/Documents/dots/.zshrc $HOME/
 
 
 echo "done, enjoy your rice."
