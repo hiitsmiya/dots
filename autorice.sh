e488a2fd33e2fc97fa28ed55eccb8bbdf2146a25
deven <56056447+notdeven@users.noreply.github.com>
2020-04-23 01:06:37 -0700

    TEST3

--- autorice.sh
+++ autorice.sh
@@ -2,5 +2,5 @@
 
 cp -r ~/Documents/dots/Wallpapers ~/Pictures/
 
-cp -r ~/Documents/dots/i3blocks ~/.config/i3blocks | chmod +x ~/.config/i3blocks/battery2.py | chmod +x ~/.config/i3blocks/volume |
+cp -r ~/Documents/dots/i3blocks ~/.config/i3blocks/ | chmod +x ~/.config/i3blocks/battery2.py | chmod +x ~/.config/i3blocks/volume |
 chmod +x ~/.config/i3blocks/corona
