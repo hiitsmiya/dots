2de6c456a2e267de47cefc5d77d501e73dcfdd08
deven <56056447+notdeven@users.noreply.github.com>
2020-04-23 00:45:25 -0700

    test3

--- autorice.sh
+++ autorice.sh
@@ -2,8 +2,7 @@
 
 cp $HOME/Documents/dots/Wallpapers $HOME/Pictures/
 
-mkdir ~/.config/i3blocks 
-cp $HOME/Documents/dots/i3blocks/ $HOME/.config/i3blocks/
+cp -r $HOME/Documents/dots/i3blocks/ $HOME/.config/i3blocks/
 chmod +x ~/.config/i3blocks/battery2.py
 chmod +x ~/.config/i3blocks/volume
 chmod +x ~/.config/i3blocks/corona
@@ -14,16 +13,16 @@ cp $HOME/Documents/dots/scripts/ $HOME/.config/scripts/
 chmod +x ~/.config/scripts/fetch
 chmod +x ~/.config/scripts/panes.sh
 chmod +x ~/.config/scripts/pipes.sh
-cp $HOME/Documents/dots/.zshrc $HOME/.zshrc
+cp -r $HOME/Documents/dots/.zshrc $HOME/.zshrc
 
-cp $HOME/Documents/dots/config $HOME/.config/i3/
+cp $-r HOME/Documents/dots/config $HOME/.config/i3/
 
-cp $HOME/Documents/dots/dunstwal /bin/
+cp -r $HOME/Documents/dots/dunstwal /bin/
 chmod +x /bin/dunstwal
-cp $HOME/Documents/dots/dunst/ $HOME/.config/
+cp -r $HOME/Documents/dots/dunst/ $HOME/.config/
 chmod +x $HOME/.config/dunst/launch.sh
 
-cp $HOME/Documents/dots/i3blocks.conf /etc/
+cp -r $HOME/Documents/dots/i3blocks.conf /etc/
 
-cp $HOME/Documents/dots/kitty.conf $HOME/.config/kitty/
+cp -r $HOME/Documents/dots/kitty.conf $HOME/.config/kitty/
 
